7748b34e617524f5295ca7043794856ef129f1da
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2021-10-31 18:26:08 +0300

    Update SideLink.js

--- src/componenets/SideLink.js
+++ src/componenets/SideLink.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const SideLink = ({ key, name, Icon, active, handleMenuItemClick }) => {
+const SideLink = ({ key, name, Icon, active, onMenuItenClick }) => {
+    const isActive = active === name;
     return (
-        <li className="group">
-            <a href={name} className="cursor-pointer block text-xl mb-2">
+        <li className="group" onClick={() => onMenuItenClick(name)}>
+            <a href={name} className="cursor-pointer block text-xl mb-2 pointer-events-none">
                 <div className="inline-block">
-                    <div className="flex items-center group:bg-primary-light group:text-primary-base rounded-full pl-3 pr-8 py-3">
+                    <div className={`flex items-center group-hover:bg-primary-light group-hover:text-primary-base rounded-full pl-3 pr-8 py-3
+                ${isActive ? "text-primary-base" : ""}
+                `}>
                         <Icon></Icon>
                         <span className="ml-4 font-bold">
                             {name}
@@ -13,7 +16,7 @@ const SideLink = ({ key, name, Icon, active, handleMenuItemClick }) => {
                     </div>
                 </div>
             </a>
-        </li>
+        </li >
 
     )
 }
